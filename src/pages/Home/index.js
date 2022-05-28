c526acb8d3470557dd3567d2bf483c1d0a2f514c
diglean <diegoleandro2002@gmail.com>
2022-05-28 09:46:40 -0300

    Home and Css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 //URL da API: /movie/now_playing?api_key=e221a4b937ed104a4fa26578f06420aa&language=pt-BR
 
@@ -18,7 +20,8 @@ function Home(){
                 }
             })
 
-            console.log(response.data.results);
+            //console.log(response.data.results.slice(0,10));
+            setFilmes(response.data.results.slice(0,10));
 
         }
 
@@ -26,8 +29,18 @@ function Home(){
     }, [])
 
     return(
-        <div>
-            <h1>Bem vindo a home</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) =>{
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt="{filme.title}"/>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
