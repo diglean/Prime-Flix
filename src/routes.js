1e03ceee1c166dc323e8f0cf3630c2ca98c0cb1c
diglean <diegoleandro2002@gmail.com>
2022-05-28 10:00:26 -0300

    Not found and loading

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,11 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+
+import Erro from './pages/Erro';
+
 import Header from './components/Header'
+
 
 function RoutesApp(){
     return(
@@ -11,6 +15,8 @@ function RoutesApp(){
             <Routes>
                 <Route path="/" element={ <Home/> }/>
                 <Route path="/filme/:id" element={ <Filme/> }/>
+
+                <Route path="*" element={ <Erro/> }/>
             </Routes>
         </BrowserRouter>
     )
