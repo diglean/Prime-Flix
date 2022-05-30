d58a9c4827d94baebff844c4ea70794dffbbf558
diglean <diegoleandro2002@gmail.com>
2022-05-30 15:19:28 -0300

    Save button

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -4,6 +4,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import './filme-info.css';
 
 import api from '../../services/api';
+import { hasFormSubmit } from '@testing-library/user-event/dist/utils';
 
 function Filme(){
 
@@ -41,6 +42,24 @@ function Filme(){
 
     }, [navigate, id])
 
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix");
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some((filmesSalvo)=> filmesSalvo.id === filme.id)
+
+        if(hasFilme){
+            alert("Esse filme já está na lista");
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        alert("Filme salvo com sucesso");
+
+    }
+
     if(loading){
         return(
             <div className='filme-info'>
@@ -59,9 +78,9 @@ function Filme(){
             <strong>Avaliação: {filme.vote_average} /10</strong>
 
             <div className="area-buttons">
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
-                    <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+                    <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
                     </a>
                 </button>
